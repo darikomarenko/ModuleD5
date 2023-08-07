a9829b9225ef06378d755aebdf75703112464ad6
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2023-08-07 18:08:42 +0300

    Add account and news apps. Create models in news app

---
+++ project/news/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Author, Post, Comment, PostCategory, Category
+
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
+admin.site.register(Category)
